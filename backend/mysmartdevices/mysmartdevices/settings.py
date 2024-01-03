201ba90917d40b1b16ea207d8fd2fef535514726
Lion1867 <127045092+Lion1867@users.noreply.github.com>
2024-01-03 22:32:35 +0500

    1

--- backend/mysmartdevices/mysmartdevices/settings.py
+++ backend/mysmartdevices/mysmartdevices/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'market'
+    'market',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DATA_DIR = os.path.join(BASE_DIR,'../init_data')
