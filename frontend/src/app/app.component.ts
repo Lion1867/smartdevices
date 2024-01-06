492ea7b084ea8eb20232240513ed092348c64ad1
Lion1867 <127045092+Lion1867@users.noreply.github.com>
2024-01-07 00:57:56 +0500

    1

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,6 +5,13 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 
+interface Product {
+  id: number;
+  name: string;
+  category: number;
+  get_small_image_url: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,12 +23,12 @@ import { HttpClientModule } from '@angular/common/http';
 export class AppComponent {
   title = 'frontend';
 
-  products = [];  
+  products: Product[] = [];   
 
   constructor(private http: HttpClient) {
     this.http.get('http://127.0.0.1:8989/v1/generic/product_list?limit=73').subscribe((data: any) => {
       console.log(data);
-      this.products = data;
+      this.products = data.results;
     });
   }
 }
