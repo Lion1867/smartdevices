ea10e95678aaf68957eae109842580ecd88e60a9
Lion1867 <127045092+Lion1867@users.noreply.github.com>
2024-01-13 15:42:51 +0500

    1

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 interface Product {
   id: number;
@@ -15,7 +16,7 @@ interface Product {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -23,7 +24,8 @@ interface Product {
 export class AppComponent {
   title = 'frontend';
 
-  products: Product[] = [];   
+  products: Product[] = [];  
+  name = ''; 
 
   constructor(private http: HttpClient) {
     this.http.get('http://127.0.0.1:8989/v1/generic/product_list?limit=73').subscribe((data: any) => {
@@ -31,4 +33,15 @@ export class AppComponent {
       this.products = data.results;
     });
   }
+
+  doFind() {
+    console.log(this.name);
+    
+    this.http.get(`http://127.0.0.1:8989/v1/generic/product_list?limit=73&searchkey=${this.name}`).subscribe((data: any) => {
+      console.log(data);
+      this.products = data.results;
+    });
+
+  }
+
 }
